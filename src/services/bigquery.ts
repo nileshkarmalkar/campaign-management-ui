6516e6bf7d8098b22f6fc11a27ac1fb1554bfbe6
nileshkarmalkar <nilesh.karmalkar@telus.com>
2025-04-24 12:17:45 -0400

    Update DataService to handle BigQuery and sample data scenarios

--- src/services/bigquery.ts
+++ src/services/bigquery.ts
@@ -1,21 +1,46 @@
 import { sampleDatasets } from '../utils/sampleData';
+import { bigQueryConfig } from '../config/bigquery';
 
 class DataService {
+  private isUsingBigQuery: boolean;
+
+  constructor() {
+    this.isUsingBigQuery = !!bigQueryConfig.projectId && !!bigQueryConfig.datasetId;
+    if (!this.isUsingBigQuery) {
+      console.warn('BigQuery credentials not provided. Using sample data instead.');
+    }
+  }
+
   async getCustomerData(): Promise<any[]> {
+    if (this.isUsingBigQuery) {
+      // TODO: Implement actual BigQuery call here
+      console.log('Fetching customer data from BigQuery');
+      // For now, we'll still return sample data
+    }
     // Simulate API delay
     await new Promise(resolve => setTimeout(resolve, 500));
     return sampleDatasets.customerData;
   }
 
   async getChurnData(): Promise<any[]> {
+    if (this.isUsingBigQuery) {
+      // TODO: Implement actual BigQuery call here
+      console.log('Fetching churn data from BigQuery');
+      // For now, we'll still return sample data
+    }
     // Simulate API delay
     await new Promise(resolve => setTimeout(resolve, 500));
     return sampleDatasets.churnModelData;
   }
 
   async executeQuery(query: string): Promise<any[]> {
-    console.log('Query to be executed:', query);
-    // For now, return customer data as a sample
+    if (this.isUsingBigQuery) {
+      console.log('Executing query on BigQuery:', query);
+      // TODO: Implement actual BigQuery query execution
+      // For now, we'll return customer data as a sample
+    } else {
+      console.log('Query to be executed (using sample data):', query);
+    }
     return this.getCustomerData();
   }
 }

21dba037ea455e365313dcadcb078893881e906a
nileshkarmalkar <nilesh.karmalkar@telus.com>
2025-04-24 11:53:20 -0400

    Add craco configuration for BigQuery Node.js polyfills

---
+++ craco.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      // Add fallback for node modules
+      webpackConfig.resolve.fallback = {
+        ...webpackConfig.resolve.fallback,
+        util: require.resolve('util/'),
+        fs: false,
+        path: require.resolve('path-browserify'),
+        buffer: require.resolve('buffer/'),
+        process: require.resolve('process/browser'),
+      };
+
+      // Add plugins
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        })
+      );
+
+      return webpackConfig;
+    },
+  },
+};

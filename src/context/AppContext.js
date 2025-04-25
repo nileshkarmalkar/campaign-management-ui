4b84019e06a68fa16e435bbf0ed77d93574bfaa4
nileshkarmalkar <nilesh.karmalkar@telus.com>
2025-04-24 20:20:24 -0400

    Fix import statement for bigquery service

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import dataService from '../services/bigquery';
+import dataService from '../services/bigquery.ts';
 
 const AppContext = createContext();
 

ed03e06f9ddd4b058262f6f79b90323875087ced
nileshkarmalkar <nilesh.karmalkar@telus.com>
2025-04-30 08:33:16 -0400

    Switch to generic theme for demo site

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 window._env_ = {
   REACT_APP_BQ_PROJECT_ID: 'aipp-tmf680-orch-dev-a613e8',
   REACT_APP_BQ_DATASET_ID: 'camp_mgmt',
-  REACT_APP_USE_TELUS_BRANDING: 'true'
+  REACT_APP_USE_TELUS_BRANDING: 'false'
 };
